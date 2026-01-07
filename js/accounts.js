b199b5eb5c4968b9f7d72e6b42fc47f7b4d7641e
unknown <juntjy@gmail.com>
2026-01-07 16:08:33 +0800

    edited most of it

--- js/accounts.js
+++ js/accounts.js
@@ -1,15 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-apiKey: "AIzaSyAwqnfRXzNVNx0CtoaBTdbn5koPkgcVc_w",
-authDomain: "my-webpage-67isded.firebaseapp.com",
-projectId: "my-webpage-67isded",
-storageBucket: "my-webpage-67isded.firebasestorage.app",
-messagingSenderId: "203044731298",
-appId: "1:203044731298:web:71eb27c97f212829ff9afb",
-measurementId: "G-6NBGPHRVEL"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
